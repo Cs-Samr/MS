05d669024bcbeac88da90e33c126013f05f1bcf0
sa-79 <140039656+sa-79@users.noreply.github.com>
2023-08-03 15:25:23 +0300

    s

--- app/Views/deskapp/ui/ui-cards.php
+++ app/Views/deskapp/ui/ui-cards.php
@@ -2132,8 +2132,8 @@
                             </div>
                         </section>
                     </div>
-                   
-                    <a class="btn btn-next" href="<?= base_url('deskapp/ui/card-hover/') ?>">Next</a>
+                    <input class="btn btn-primary btn-lg btn-block" type="submit" value="Next"  href="http://localhost/MS/deskapp/ui/card-hover" class="btn" >
+                    
                 </form>
             </div>
         </div>
