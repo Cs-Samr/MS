b210da72a33e044782cf3d6941205254c3da8b71
Cs-Samr <smr.aff.alh@gmail.com>
2023-08-03 10:26:03 +0300

    go

--- app/Views/deskapp/auth/reset-password.php
+++ app/Views/deskapp/auth/reset-password.php
@@ -35,7 +35,7 @@
 		<div class="container-fluid d-flex justify-content-between align-items-center">
 			<div class="brand-logo">
 				<a href="login.html">
-					<img src="vendors/images/logo-dark.png" alt="">
+					<img src="vendors/images/logo-white.png" alt="">
 				</a>
 			</div>
 			<div class="login-menu">
