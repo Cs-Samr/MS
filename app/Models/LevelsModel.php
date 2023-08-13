c811a0e45215db5da6855f32d158502088b39dab
Cs-Samr <smr.aff.alh@gmail.com>
2023-08-13 17:06:14 +0300

    go yes

--- app/Models/LevelsModel.php
+++ app/Models/LevelsModel.php
@@ -10,9 +10,21 @@ class LevelsModel extends Model
     protected $primaryKey = 'id_level';
     protected $allowedFields = ['title', 'details', 'id_mem', 'id_project' ,'d_start','d_end','level#','states'];
 
-    public function getuser()
+    public function getlevel()
     {
-        return $this->findAll();
+        //return $this->findAll();
+        return $this->select('*')->findAll();
+
+    }
+
+    public function saveLevel($data)
+    {
+        // $projectCode = $data['id_project'] . $data['pro_name'];
+        // $data['project_code'] = $projectCode;
+
+        //unset($data['id_project']); // Remove the id_project field from the data array as it's not needed now
+
+        return $this->insert($data);
     }
 }
 
